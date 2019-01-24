ecd023c2915e5c3b85614d857c916cd6a2b36316
Ousama Elayan <ousamasama@gmail.com>
2019-01-24 15:22:32 -0600

    added four tests following walkthrough in class

--- music/history/tests.py
+++ music/history/tests.py
@@ -20,4 +20,24 @@ class ArtistTest(TestCase):
 
         self.assertIn(new_artist.artist_name.encode(), response.content)
 
+    def test_get_artist_form(self):
+        response = self.client.get(reverse('history:addartist'))
 
+        self.assertIn(
+            '<input type="text" name="artist_name" maxlength="100" required id="id_artist_name">'.encode(), response.content
+        )
+
+    def test_post_artist(self):
+        response = self.client.post(reverse('history:addartist'), 
+            {'artist_name': 'Corgi Fur'})
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_artist_detail(self):
+        new_artist = Artist.objects.create(
+            artist_name = "Corgi Fur"
+        )
+
+        response = self.client.get(reverse('history:detail', args=(1,)))
+        print("response", response)
+        self.assertEqual(response.context["artist"].artist_name, new_artist.artist_name)
